c6bad413860f98e7bcd86444d15b3c2ffc2f3daf
zhou467778 <zhou.wann@husky.neu.edu>
2020-03-26 16:47:29 -0400

    changed URL from http to https

--- src/app/services/CourseServiceClient.ts
+++ src/app/services/CourseServiceClient.ts
@@ -3,11 +3,11 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class CourseServiceClient {
   findAllCourses = () =>
-    fetch('http://wbdv-generic-server.herokuapp.com/api/zhou.wann/courses')
+    fetch('https://wbdv-generic-server.herokuapp.com/api/zhou.wann/courses')
       .then(response => response.json())
 
   findCourseById = (courseId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/zhou.wann/courses/${courseId}`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/zhou.wann/courses/${courseId}`)
       .then(response => response.json())
 }
 
--- src/app/services/ModuleServiceClient.ts
+++ src/app/services/ModuleServiceClient.ts
@@ -3,6 +3,6 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class ModuleServiceClient {
   findModulesForCourse = (courseId) =>
-    fetch(`http://wbdv-generic-server.herokuapp.com/api/zhou.wann/courses/${courseId}/modules`)
+    fetch(`https://wbdv-generic-server.herokuapp.com/api/zhou.wann/courses/${courseId}/modules`)
       .then(response => response.json())
 }
