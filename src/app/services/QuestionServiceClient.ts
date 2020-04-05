19fa485bd10e9f31456af3932a6b3127434b3d1f
zhou467778 <zhou.wann@husky.neu.edu>
2020-04-04 21:05:27 -0400

    replace URL to heroku URL

--- src/app/services/QuestionServiceClient.ts
+++ src/app/services/QuestionServiceClient.ts
@@ -3,7 +3,7 @@ import {Injectable} from '@angular/core';
 Injectable()
 export class QuestionServiceClient {
   findQuestionsForQuiz = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}/questions`)
+    fetch(`http://cs4550-server-node.herokuapp.com/api/quizzes/${qid}/questions`)
       .then(response => response.json())
 
 }
--- src/app/services/QuizServiceClient.ts
+++ src/app/services/QuizServiceClient.ts
@@ -3,11 +3,11 @@ import {Injectable} from '@angular/core';
 @Injectable()
 export class QuizServiceClient {
   findAllQuizzes = () =>
-    fetch(`http://localhost:3000/api/quizzes`)
+    fetch(`http://cs4550-server-node.herokuapp.com/api/quizzes`)
       .then(response => response.json())
 
   findQuizById = (qid) =>
-    fetch(`http://localhost:3000/api/quizzes/${qid}`)
+    fetch(`http://cs4550-server-node.herokuapp.com/api/quizzes/${qid}`)
       .then(response => response.json())
 
 }
